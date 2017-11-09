c9b66ec29f68f07aff74b8cd18f5a6503e2800b8
tananta21 <terre.21az@gmail.com>
2017-11-09 02:48:56 -0500

    updates in dashboard

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+//Route::get('/mapa', function () {
+//    return view('mapa');
+//});
+//
+//
---
+++ resources/views/mapa.blade.php
@@ -0,0 +1,6 @@
+@extends('index')
+@section('content')
+<div>
+
+</div>
+@endsection
